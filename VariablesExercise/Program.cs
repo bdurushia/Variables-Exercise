25d23204308767b93c080c0f22161104047213ba
Bryan Durushia <b.durushia@gmail.com>
2024-02-15 11:57:41 -0600

    Declared and initialized different variable types. Used string interpolation to write variable values within strings to the console

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Bryan Durushia";
+
+            int myAge = 29;
+
+            char myFirstInitial = 'B';
+            char myLastInitial = 'D';
+
+            bool isMarried = false;
+
+            decimal cash = 76.54m;
+            double exactAge = 29.53;
+
+            Console.WriteLine($"My name is {myName}.\nI am {myAge} years old, but my exact age is {exactAge}.\nMy initials are {myFirstInitial}{myLastInitial}.");
+            Console.WriteLine($"If you'd assume that I am married, that is {isMarried}.\nI have ${cash} in my wallet.");
         }
     }
 }
